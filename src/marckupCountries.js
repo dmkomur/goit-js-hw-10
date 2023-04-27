f5df14d9fc7efb5705184fc306919c26d214a10f
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-04-27 21:23:32 +0300

    fix const

--- src/marckupCountries.js
+++ src/marckupCountries.js
@@ -1,11 +1,11 @@
 "use strict"
 export function markupArray(array) {
-    return stingsArray = array.map(e => 
+    return array.map(e => 
     {
         return `<li class="listEl"><img height="25" src=${e.flags.svg}> <p class="listText">${e.name.official}</p></li>`
     }).join('')
 }
 
 export function markupCard(e) {
-        return ` <div class="cardEl"><img height="50" src=${e[0].flags.svg}><p class="cardText">${e[0].name.official}</p><p class="cardText">capital: ${e[0].capital}</p><p class="cardText">languages: ${Object.values(e[0].languages)}</p><p class="cardText">population: ${e[0].population}</p></div> `
+        return ` <div class="cardEl"><img height="50" src=${e[0].flags.svg}><p class="cardText">${e[0].name.official}</p><p class="cardText">capital: ${e[0].capital}</p><p class="cardText">languages: ${Object.values(e[0].languages).join(', ')}</p><p class="cardText">population: ${e[0].population}</p></div> `
 }
