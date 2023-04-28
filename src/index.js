1df088dd54dbaa656d36df520b441391cb94a88d
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-04-28 11:12:06 +0300

    fixet order

--- src/index.js
+++ src/index.js
@@ -21,9 +21,14 @@ function onInputChange(e
     }
     fetchCountries(searchQuery).then(data => {
         if (data.length > 10) { Notiflix.Notify.success("Too many matches found. Please enter a more specific name."); }
-        else if (data.length === 1) { countryCardRef.innerHTML = `${markupCard(data)}`; countryListRef.innerHTML = ''}
-        else { countryListRef.innerHTML = `${markupArray(data)}`; countryCardRef.innerHTML = ''}
-    } ).catch(err => Notiflix.Notify.failure("Oops, there is no country with that name"));
+        else if (data.length === 1) { countryListRef.innerHTML = ''; countryCardRef.innerHTML = `${markupCard(data)}`; }
+        else { countryCardRef.innerHTML = ''; countryListRef.innerHTML = `${markupArray(data)}`; }
+    }).catch(err => {
+        countryListRef.innerHTML = '';
+        countryCardRef.innerHTML = '';
+
+        Notiflix.Notify.failure("Oops, there is no country with that name")
+    });
 }
 
 
